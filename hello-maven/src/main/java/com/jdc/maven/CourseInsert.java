bf4ce9127cbd7f2be0a33c572cc1e2b315ecd128
Ye Min Ko <yeminko47@gmail.com>
2022-03-08 15:53:10 +0630

    [added] bean config and IoC container

--- hello-maven/src/main/java/com/jdc/maven/CourseInsert.java
+++ hello-maven/src/main/java/com/jdc/maven/CourseInsert.java
@@ -5,19 +5,17 @@ import java.sql.SQLException;
 
 import javax.sql.DataSource;
 
+import com.mysql.cj.jdbc.MysqlDataSource;
+
 public class CourseInsert {
+
+	private static final String SQL = "insert into course (name, fees, duration) values (?,?,?)";
 
 	private DataSource dataSource;
 
 	public CourseInsert(DataSource dataSource) {
-		super();
 		this.dataSource = dataSource;
 	}
-
-//	private static final String USER = "root";
-//	private static final String PASSWORD = "root";
-//	private static final String URL = "jdbc:mysql://localhost:3306/hello_maven";
-	private static final String SQL = "insert into course (name, fees, duration) values (?,?,?)";
 
 	public void insert(Course c) {
 		try (var conn = dataSource.getConnection(); var stmt = conn.prepareStatement(SQL)) {
--- hello-maven/src/test/java/com/jdc/test/HelloTest.java
+++ hello-maven/src/test/java/com/jdc/test/HelloTest.java
@@ -1,12 +1,27 @@
 package com.jdc.test;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.jdc.maven.Course;
 import com.jdc.maven.CourseInsert;
 
 public class HelloTest {
 	
+	private static ClassPathXmlApplicationContext IOC_CONTAINER;
+	
+	@BeforeAll
+	static void initialize() {
+		IOC_CONTAINER = new ClassPathXmlApplicationContext("classpath:/app-config.xml");
+	}
+	
+	@AfterAll
+	static void close() {
+		IOC_CONTAINER.close();
+	}
+
 	@Test
 	void test() {
 		var c = new Course();
@@ -14,7 +29,7 @@ public class HelloTest {
 		c.setFees(200000);
 		c.setDuration(4);
 		
-		var insert = new CourseInsert();
+		var insert = IOC_CONTAINER.getBean("insert", CourseInsert.class);
 		insert.insert(c);
 	}
 }
